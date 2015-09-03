8c6e2b2f673ca21460aa94f00a7e1dc694d6fbb8
Jaakko Leikko <jaakko.leikko@haaga-helia.fi>
2015-09-03 23:38:51 +0300

    first training made with leiningen

---
+++ t1-try/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to t1-try
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ t1-try/src/t1_try/core.clj
@@ -0,0 +1,7 @@
+(ns t1-try.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
